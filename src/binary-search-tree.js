ed3e107d73653277001e2713277ae6bda7602f9e
Andrey_Miller <andrey_miller01@list.ru>
2023-04-17 20:10:53 +0400

    feat: 15/20 binary search

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,41 +6,158 @@ const { NotImplementedError } = require('../extensions/index.js');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
 class BinarySearchTree {
+  constructor() {
+    this.currentNode = null;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.currentNode;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  add(data) {
+    var newNode = new Node(data);
+    if (this.currentNode === null) this.currentNode = newNode;
+    else this.insertNode(this.currentNode, newNode);
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  insertNode(node, newNode) {
+    if (newNode.data < node.data) {
+      if (node.left === null) node.left = newNode;
+      else this.insertNode(node.left, newNode);
+    }
+
+    else {
+      if (node.right === null) node.right = newNode;
+      else this.insertNode(node.right, newNode);
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    if (this.currentNode === null) {
+      return false;
+    }
+  
+    function search(node, data) {
+      if (node === null) {
+        return null;
+      } else if (data < node.data) {
+        return search(node.left, data);
+      } else if (data > node.data) {
+        return search(node.right, data);
+      } else {
+        return true;
+      }
+    }
+  
+    return search(this.currentNode, data);
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    if (this.currentNode === null) {
+      return null;
+    }
+  
+    function search(node, data) {
+      if (node === null) {
+        return null;
+      } else if (data < node.data) {
+        return search(node.left, data);
+      } else if (data > node.data) {
+        return search(node.right, data);
+      } else {
+        return node;
+      }
+    }
+  
+    return search(this.currentNode, data);
   }
 
+  remove(data){
+      this.currentNode = this.removeNode(this.currentNode, data);
+  }
+   
+  removeNode(node, key)
+  {
+           
+
+      if(node === null)
+          return null;
+   
+      else if(key < node.data)
+      {
+          node.left = this.removeNode(node.left, key);
+          return node;
+      }
+   
+
+      else if(key > node.data)
+      {
+          node.right = this.removeNode(node.right, key);
+          return node;
+      }
+   
+      else
+      {
+          if(node.left === null && node.right === null)
+          {
+              node = null;
+              return node;
+          }
+   
+          if(node.left === null)
+          {
+              node = node.right;
+              return node;
+          }
+           
+          else if(node.right === null)
+          {
+              node = node.left;
+              return node;
+          }
+   
+          var aux = this.findMinNode(node.right);
+          node.data = aux.data;
+   
+          node.right = this.removeNode(node.right, aux.data);
+          return node;
+      }
+   
+  }
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.currentNode === null) {
+      return null;
+    }
+
+    let current = this.currentNode;
+    while (current.left != null) {
+      current = current.left;
+    }
+
+    return current.data;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.currentNode === null) {
+        return null;
+      }
+  
+      let current = this.currentNode;
+      while (current.right != null) {
+        current = current.right;
+      }
+  
+      return current.data;
   }
 }
 
